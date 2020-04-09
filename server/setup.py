3da7d936e2fde17878fd3015fc1dd3a041ca0af0
Edoardo <edoardo-baldini@virgilio.it>
2020-04-09 17:54:45 +0200

    setup issue

--- server/setup.py
+++ server/setup.py
@@ -4,8 +4,10 @@ setup(
     name='IoT Green Calculator',
 
     keywords='rest restful api flask swagger openapi flask-restplus',
+    url='https://github.com/edoBaldini/IoTGreenCalc',
+    author="Edoardo Baldini",
 
     packages=find_packages(),
 
-    install_requires=['flask-restplus==0.9.2'],
+    install_requires=['flask-restplus==0.9.2', 'Flask==1.1.1', 'Flask-Cors==3.0.7'],
 )
