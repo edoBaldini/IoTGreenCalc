872537d141c663ba0870e06db9f3161fa7a518e3
Edoardo <edoardo-baldini@virgilio.it>
2020-04-10 15:53:31 +0200

    empty master

--- server/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from .home import home
-from .device import device
-from .solar_panel import solar_panel
-from .battery import battery
-from .maintenance import maintenance
-
-blueprints = [home, device, solar_panel, battery, maintenance]
